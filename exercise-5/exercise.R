9eb7cdfaf9ab37d4ebe8862c912ec99d50389184
Mitch Deamon <deamon19@uw.edu>
2016-10-27 14:26:40 -0700

    m10-apis

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -10,6 +10,7 @@ GetReview <- function(movie)
   # Construct a search query using YOUR api key
   # The base URL is https://api.nytimes.com/svc/movies/v2/reviews/search.json?
   # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
+  
   base.url <- 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
   api.key <- 'f9339895b12c497699f1cf4b3e4a8b47'
   
@@ -21,11 +22,12 @@ GetReview <- function(movie)
   # Flatten the data stored in the `$results` key of the data returned to you
   flattened <- flattened[1,]
   headline <- first.review$headline
-  summary <- first.review$headline$summary
+  summary <- first.review$headline$summary_short
+  link <- first.review$link.url
   
   
   # From the most recent review, store the headline, short summary, and link to full article each in their own variables
-  
+  info <- list(headline=headline, summary=summary, link=link)
   
   # Return an list of the three pieces of information from above
   
